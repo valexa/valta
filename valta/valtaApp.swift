1a950d5b5814265f7b902ce0ca64c472c3f4d66c
Vlad Alexa <git@vladalexa.com>
2025-12-04 07:18:28 +0000

    xcode skeleton

---
+++ valta/valtaApp.swift
@@ -0,0 +1,17 @@
+//
+//  valtaApp.swift
+//  valta
+//
+//  Created by vlad on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct valtaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
